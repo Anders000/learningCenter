792a285a499388927acd3d2e5015fca653f14da4
root <andersoncalefi.almeida@gmail.com>
2022-03-22 01:01:03 -0300

    api MB learning

--- js_mercadoBitcoin/main.js
+++ js_mercadoBitcoin/main.js
@@ -1,17 +1,32 @@
 var moedaForm = document.getElementById('coinForm');
 
-moedaForm.addEventListener('change', function(){
+moedaForm.addEventListener('change', function () {
   console.log(this.value)
-  const urlApi = 'https://www.mercadobitcoin.net/api/'+ this.value +'/ticker/';
+  const urlApiTicker = 'https://www.mercadobitcoin.net/api/' + this.value + '/ticker/';
+  const urlApiData = 'https://www.mercadobitcoin.net/api/' + this.value + '/day-summary/2018/6/20/';
 
-  
-fetch(urlApi).then(function(response) {
-  response.json().then(function(data) {    
-  var coin = data.ticker
-    console.log(coin.vol);
-  });
-}).catch(function(err) {
-  console.error('Failed retrieving information', err);
-})
+  fetch(urlApiTicker).then(function (response) {
+    response.json().then(function (data) {
+      var coin = data.ticker
+      console.log(coin.high);
+      console.log(coin.low);
+      console.log(coin.vol);
+      console.log(coin.last);
+      console.log(coin.buy);
+      console.log(coin.sell);
+    });
+  }).catch(function (err) {
+    console.error('Failed retrieving information', err);
+  })
+  var headers = {}
+
+  fetch(urlApiData).then(function (response) {
+    response.json().then(function (data) {
+      var coinData = data
+      console.log(coinData.amount + ' working no cors problem');
+    });
+  }).catch(function (err) {
+    console.error('Failed retrieving information', err);
+  })
 
 })
